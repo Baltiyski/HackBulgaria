00930e0005f63eb8a04cef9470a1ba06bc783acd
StefanBaltiyski <stefan.baltiyski@gmai.com>
2015-03-08 16:40:25 +0200

    initial commit

---
+++ Programming0-1/week02/01-Basic-List-Operations/firstArray.py
@@ -0,0 +1,8 @@
+one = [1];
+two = [1, 2, 3];
+nullArray = [];
+boolArray = [True, True, True];
+smoeArray = ["Python", "Django"];
+myName = ["Stefan", "Plamenov", "Baltiyski"];
+friendsArr = ["Vitan", "Ivailo", "Dimityr", "Radina", "Stoimen"];
+languageArr = ["Java", "C#", "C++", "Prolog", "HTML", "CSS", "Python", "SQL", "PL/SQL", "MySQL" ".."];
